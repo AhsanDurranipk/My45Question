389927950514294ca17214bf1d4cd40234760241
AhsanDurrani <124517411+AhsanDurranipk@users.noreply.github.com>
2024-02-26 02:38:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question no: 1
+console.log("Hello world!");
+// Question no: 2
+var myName = 'Ahsan';
+var message = 'is a good person';
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question no: 1
+console.log("Hello world!");
+
+// Question no: 2
+
+let myName = 'Ahsan';
+let message = 'is a good person';
+console.log(myName+" "+message);
